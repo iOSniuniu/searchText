cbec17178c838aa0303e53d58d97ec69b6078b62
niuniu232 <1145717295@qq.com>
2017-12-13 17:34:14 +0800

    Initial Commit

---
+++ searchText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  searchText
+//
+//  Created by apple on 2017/12/13.
+//  Copyright © 2017年 牛道健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
